5783f187e4fb90e1034fbb6070950fa9e9d369a0
jackforevergreen <jack@forevergreen.earth>
2023-11-18 05:32:04 -0500

    Initial Commit

---
+++ FGapp1/FGapp1App.swift
@@ -0,0 +1,17 @@
+//
+//  FGapp1App.swift
+//  FGapp1
+//
+//  Created by Jack Pearson on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct FGapp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
